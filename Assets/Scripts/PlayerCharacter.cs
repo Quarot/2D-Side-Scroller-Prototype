3bac690dc552618cdeef08bdc00993beb6a63b9d
Stephen Kellogg <Stephen.Kellogg@iam.local>
2018-10-10 15:16:48 -0500

    Got to starting on checkpoint
    Put in hazard, crate, jump collision, up to video 10

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,35 +5,75 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour {
     [SerializeField]
     private float accelerationForce = 5;
-
-    [SerializeField]
-    private Rigidbody2D rb2d;
     [SerializeField]
     private float maxSpeed = 5;
     [SerializeField]
     private float jumpForce = 10;
-    private float horizontalInput;
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private Collider2D playerGroundCollider;
+
+    [SerializeField]
+    private PhysicsMaterial2D playerMovingPhysicsMaterial, playerStoppingPhysicsMaterial;
+
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
 
     [SerializeField]
     private ContactFilter2D groundContactFilter;
 
+    private float horizontalInput;
     private bool isOnGround;
-	
-	
-	// Update is called once per frame
-	void Update () {
-        horizontalInput = Input.GetAxis("Horizontal");
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
+    private Checkpoint currentCheckpoint;
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-	}
+    // Update is called once per frame
+    void Update ()
+    {
+        UpdateIsOnGround();
+        UpdateHorizontalInput();
+        HandleJumpInput();
+    }
+
+    private void UpdateHorizontalInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
 
     private void FixedUpdate()
     {
+        UpdatePhysicsMaterial();
         Move();
 
+    }
+
+    private void UpdatePhysicsMaterial()
+    {
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            playerGroundCollider.sharedMaterial = playerMovingPhysicsMaterial;
+        }
+        else
+        {
+            playerGroundCollider.sharedMaterial = playerStoppingPhysicsMaterial;
+        }
+    }
+
+    private void UpdateIsOnGround()
+    {
+        isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+        //Debug.Log("IsOnGround?: " + isOnGround);
+
+    }
+
+    private void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     private void Move()
@@ -42,5 +82,10 @@ public class PlayerCharacter : MonoBehaviour {
         Vector2 clampedVelocity = rb2d.velocity;
         clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
         rb2d.velocity = clampedVelocity;
+    }
+
+    private void SetCurrentCheckpoint(Checkpoint newCurrentCheckpoint)
+    {
+        currentCheckpoint = newCurrentCheckpoint;
     }
 }
