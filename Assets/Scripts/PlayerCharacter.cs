f7a56ef0c5c916ff1ed4ed7884dfda8f95110b05
Stephen Kellogg <Stephen.Kellogg@iam.local>
2018-10-10 13:26:09 -0500

    Copied Everything over?

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+    [SerializeField]
+    private float accelerationForce = 5;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float maxSpeed = 5;
+    [SerializeField]
+    private float jumpForce = 10;
+    private float horizontalInput;
+
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
+    private bool isOnGround;
+	
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        rb2d.AddForce(Vector2.right * horizontalInput * accelerationForce);
+        Vector2 clampedVelocity = rb2d.velocity;
+        clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = clampedVelocity;
+    }
+}
