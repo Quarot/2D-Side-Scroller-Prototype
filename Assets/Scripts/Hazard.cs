0b231a3a28cf92fb5b97dea9d5598598932b6c5d
Quarot <corneljackson1@gmail.com>
2018-12-05 13:00:09 -0600

    Returned scripts to program

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour {
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Player entered the hazard.");
+        //SoundManagerScript.PlaySound("death");
+        //SoundManagerScript.PlaySound("playerDies");
+        PlayerCharacter player = collision.gameObject.GetComponent<PlayerCharacter>();
+        //player.Respawn();
+        player.KillPlayer();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the hazard.");
+            //SoundManagerScript.PlaySound("death");
+            //SoundManagerScript.PlaySound("playerDies");
+            PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+            //player.Respawn();
+            player.KillPlayer();
+            
+        }
+        else
+        {
+        Debug.Log("Something other than player entered the hazard.");
+
+        }
+    }
+}
